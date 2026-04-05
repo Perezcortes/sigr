6abbe86b6016627c6defe027c7a951e8702ca1a3
Carlos Ernesto Millán <encuadrante@gmail.com>
2026-04-05 00:52:45 -0600

    routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\LeadWebhookController;
 use App\Http\Controllers\Api\OwnerTenantProfileController;
+use App\Http\Controllers\Api\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 // Rutas públicas
@@ -19,5 +20,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/profile/owner', [OwnerTenantProfileController::class, 'updateOwner']);
     Route::get('/profile/tenant', [OwnerTenantProfileController::class, 'showTenant']);
     Route::put('/profile/tenant', [OwnerTenantProfileController::class, 'updateTenant']);
+
+    Route::get('/properties', [PropertyController::class, 'index']);
+    Route::get('/properties/{property}', [PropertyController::class, 'show']);
 });
 
