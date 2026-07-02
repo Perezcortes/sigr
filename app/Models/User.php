f5ed3cae6445e2a2f6a79e59aab45cd43267ed1c
joel-matias <masj050829@gs.utm.mx>
2026-07-02 08:29:10 -0600

    feat: mueve las preferencias de notificación de rents a users

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,6 +61,18 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, HasMedia
         'asesor_id',
         'score',
         'evolution_whatsapp_instance_id',
+        'notif_recordatorios_email',
+        'notif_recordatorios_push',
+        'notif_recordatorios_whatsapp',
+        'notif_reporte_pago_email',
+        'notif_reporte_pago_push',
+        'notif_reporte_pago_whatsapp',
+        'notif_mensajes_email',
+        'notif_mensajes_push',
+        'notif_mensajes_whatsapp',
+        'notif_mantenimiento_email',
+        'notif_mantenimiento_push',
+        'notif_mantenimiento_whatsapp',
     ];
 
     /**
@@ -90,6 +102,18 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, HasMedia
             'is_buyer' => 'boolean',
             'is_seller' => 'boolean',
             'score' => 'integer',
+            'notif_recordatorios_email' => 'boolean',
+            'notif_recordatorios_push' => 'boolean',
+            'notif_recordatorios_whatsapp' => 'boolean',
+            'notif_reporte_pago_email' => 'boolean',
+            'notif_reporte_pago_push' => 'boolean',
+            'notif_reporte_pago_whatsapp' => 'boolean',
+            'notif_mensajes_email' => 'boolean',
+            'notif_mensajes_push' => 'boolean',
+            'notif_mensajes_whatsapp' => 'boolean',
+            'notif_mantenimiento_email' => 'boolean',
+            'notif_mantenimiento_push' => 'boolean',
+            'notif_mantenimiento_whatsapp' => 'boolean',
         ];
     }
 
